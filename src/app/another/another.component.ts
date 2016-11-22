c0a50c4349738f56f7a74951b51e050866424b5e
IIIJIaHr <midav.lis@gmail.com>
2016-11-22 21:26:55 +0300

    Migrate from orders.component to its parent another.component

--- src/app/another/another.component.ts
+++ src/app/another/another.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../services/api.service.ts'
+
 
 @Component({
   selector: 'another',
@@ -6,4 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./another.style.scss']
 })
 export class AnotherPage {
+  private orders: any;
+
+  constructor(private apiService: ApiService,
+              private router: Router) {}
+  
+  ngOnInit() {
+    if(!localStorage.getItem('authToken')) {
+      this.router.navigate(['/login']);
+    }
+
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.apiService
+      .getOrders()
+      .then( orders => this.orders = orders);
+  }
 }
--- src/app/another/orders/orders.ts
+++
@@ -1,8 +0,0 @@
-import { Order } from './order.ts'
-
-export class Orders {
-  count: number;
-  next: string;
-  previous: string;
-  results: Order[];
-}
