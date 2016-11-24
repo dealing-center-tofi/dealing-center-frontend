9545a123b821b875700468a2b1d470c3e7f7d969
IIIJIaHr <midav.lis@gmail.com>
2016-11-25 00:09:51 +0300

    implemented updating last_value through websockets

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,29 +4,39 @@ import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 
 import { ApiService } from '../services/api.service.ts'
+import { CurrencyService } from '../services/currency.service.ts'
+
 const config = require('./../../../config/api.conf');
 
 @Component({
   selector: 'dashboard',
-  templateUrl: './dashboard.template.html'
+  templateUrl: './dashboard.template.html',
+  styleUrls: ['./dashboard.style.scss']
 })
 export class Dashboard {
   currencyPairs;
   selectedPair;
+  events = {
+    'authorized': this.onAuthorize
+    'new values': this.onNewValues
+  };
+  token;
 
   constructor(
+    private currencyService: CurrencyService,
     private apiService: ApiService,
     private router: Router) {}
 
   ngOnInit() {
-    if(!localStorage.getItem('authToken')) {
+    this.token = localStorage.getItem('authToken');
+    if(!this.token) {
       this.router.navigate(['/login']);
     }
-
     this.getCurrencyPairs();
+    this.configurateSocket();
   }
 
-   createOrder(type, initialAmount) {
+  createOrder(type, initialAmount) {
     if(!this.selectedPair.id && !type && !initialAmount) return;
     this.apiService.createOrder(this.selectedPair.id, +type, +initialAmount);
   }
@@ -45,4 +55,43 @@ export class Dashboard {
     this.selectedPair = pair;
   }
 
+  configurateSocket() {
+    this.currencyService.messages.subscribe( msg => this.onMessage.apply(this, [msg]));
+    this.currencyService.messages.next(JSON.stringify(['authorize', {'token': this.token}]));
+    this.currencyService.messages.next(JSON.stringify(['subscribe', {}]));
+  }
+
+  copyValues(oldValues, newValues) {
+    if(!oldValues || !newValues) return;
+    for(let i = 0; i < oldValues.length; i++){
+      oldValues[i].last_value.isUpBid = this.isBigger(newValues[i].bid, oldValues[i].last_value.bid);
+      oldValues[i].last_value.isUpAsk = this.isBigger(newValues[i].ask, oldValues[i].last_value.ask);
+      oldValues[i].last_value.isDownBid = this.isSmaller(newValues[i].bid, oldValues[i].last_value.bid);
+      oldValues[i].last_value.isDownAsk = this.isSmaller(newValues[i].ask, oldValues[i].last_value.ask);
+      oldValues[i].last_value.bid = newValues[i].bid;
+      oldValues[i].last_value.ask = newValues[i].ask;
+    }
+  }  
+  
+  isBigger(a, b) {
+    return (a - b) < 0;
+  }
+
+  isSmaller(a, b) {
+    return (a - b) > 0;
+  }
+
+
+  onAuthorize(msg) {
+    console.log('authorized');
+  }
+
+  onNewValues(msg) {
+    if (!this.currencyPairs) return;
+    this.copyValues(this.currencyPairs.results, msg.data[0]);
+  }
+
+  onMessage(msg) {
+    this.events[msg.eventName].apply(this, [msg]);
+  }
 }
