c75641eadd5975b2d8bc84c28e538960bf6475c7
IIIJIaHr <midav.lis@gmail.com>
2016-12-03 21:39:39 +0300

    add validation(reactive form)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, AfterViewChecked } from '@angular/core';
-import { Validators, NgForm } from '@angular/forms';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
@@ -16,53 +16,28 @@ declare var jQuery;
   templateUrl: './dashboard.template.html',
   styleUrls: ['./dashboard.style.scss']
 })
-export class Dashboard implements AfterViewChecked {
-  createOrderForm: NgForm;
+export class Dashboard {
+  createOrderForm: FormGroup;
   currencyPairs;
   selectedPair;
   orderType;
-  radioModel = "Middle";
   token;
   hideOrderSuccess = true;
   hideOrderError = true;
-  @ViewChild('createOrderForm') currentForm: NgForm;
 
   constructor(private webSocketService: WebSocketService,
               private apiService: ApiService,
-              private router: Router){
-    //          private formBuilder: FormBuilder) {
-    //this.createOrderForm = formBuilder.group({
-    //  'amount': [null, Validators.compose([Validators.required, Validators.pattern('(?:\d*\.)?\d+]')])],
-    //  'order': [null, Validators.required],
-    //  'gender': [null, Validators.required]
-    //});
-    //this.createOrderForm.valueChanges.subscribe( (form: any) => {
-    //  console.log('form', form);
-    // });
+              private router: Router,
+              private formBuilder: FormBuilder) {
+    this.createOrderForm = formBuilder.group({
+      'amount': [null, Validators.compose([Validators.required, Validators.pattern('(?:\d*\.)?\d+')])],
+      'order-type': [null, Validators.required],
+    });
+    this.createOrderForm.valueChanges.subscribe( (form: any) => {
+      this.orderType = form['order-type'];
+      console.log('form', form);
+     });
   }
-
-  ngAfterViewChecked() {
-    this.formChanged();
-  }
-
-  toggleButton(element) {
-
-  }
-
-  formChanged() {
-    if (this.currentForm === this.createOrderForm) { return; }
-    this.createOrderForm = this.currentForm;
-    if (this.createOrderForm) {
-      this.createOrderForm.valueChanges
-        .subscribe(data => this.onValueChanged(data));
-    }
-  }
-
-  onValueChanged(data?: any) {
-    if (!this.createOrderForm) { return; }
-    const form = this.createOrderForm.form;
-    console.log(form.status);
-    }
 
   ngOnInit() {
     this.token = localStorage.getItem('authToken');
@@ -111,16 +86,6 @@ export class Dashboard implements AfterViewChecked {
         this.currencyPairs.results[i].last_value = res[i];
       }
     });
-  }
-
-  copyValues(oldValues, newValues) {
-    if (!oldValues || !newValues) return;
-    for (let i = 0; i < oldValues.length; i++) {
-      oldValues[i].last_value.isUpBid = this.isBigger(newValues[i].bid, oldValues[i].last_value.bid);
-      oldValues[i].last_value.isUpAsk = this.isBigger(newValues[i].ask, oldValues[i].last_value.ask);
-      oldValues[i].last_value.bid = newValues[i].bid;
-      oldValues[i].last_value.ask = newValues[i].ask;
-    }
   }
 
   isBigger(a, b) {
